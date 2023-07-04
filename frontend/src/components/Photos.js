e6f8000b9593f0f162020ee265bc7f0c76951793
Bhaskar Kapri <bhaskarkapri07@gmail.com>
2023-07-04 18:28:20 +0530

    add logic for fetching images and refacator

--- frontend/src/components/Photos.js
+++ frontend/src/components/Photos.js
@@ -1,29 +1,30 @@
 import { useEffect, useState } from 'react'
 import axios from 'axios'
 
-const Photos = () => {
+const Photos = ({ prompt }) => {
     const [photos, setPhotos] = useState([])
 
     useEffect(() => {
-        const fetchPhotos = async () => {
-            try {
-                const response = await axios.get('dummy') //dummy here will be replaced by the api url from where the images will be fetched
-                setPhotos(response.data.photos)
-            } catch (error) {
-                console.error('Error fetching images: ', error)
-            }
+        const fetchImages = () => {
+            axios
+                .post('Imagen Api url', { prompt })
+                .then((response) => setPhotos(response.data.photos))
+                .catch((error) =>
+                    console.error('Error fetching images: ', error)
+                )
         }
 
-        const interval = setInterval(fetchPhotos, 15000)
+        fetchImages()
+        const interval = setInterval(fetchImages, 15000)
 
         return () => clearInterval(interval)
-    }, [])
+    }, [prompt])
 
     return (
         <div className='photo-container'>
-            {photos.map((photo) => (
+            {photos.map((photo, index) => (
                 <img
-                    key={photo.id}
+                    key={index}
                     className='photo'
                     src={photo.url}
                     alt={photo.title}
